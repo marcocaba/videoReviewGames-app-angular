fafa29fe0c63ec37d90392a0f47c05e0f28fe463
Mnq2610 <mario.nievaquintana@gmail.com>
2025-06-11 19:29:08 +0200

    fixed footer and end

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,21 +1,36 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiServiceUsersService } from '../api-service-users.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
-  constructor(private router: Router) {
+  logged: boolean = false;  
 
+  constructor(private router: Router, private apiServiceUsers: ApiServiceUsersService) {
+    this.isLogged();
+  }
+
+  isLogged() {
+    this.apiServiceUsers.checkLogInState().subscribe({
+      next: response => {
+        this.logged = response
+      },
+      error: error => {
+        console.log(error);
+      }
+    })
   }
 
 
   signIn(){
-    this.router.navigate(['/user'])
+    this.router.navigate(['/login'])
   }
 }
